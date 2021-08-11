e612cec86c2d37d804c561e64a5de16607804add
XOLISANI NGCOBO <38984822+IamXolisaniNgcobo@users.noreply.github.com>
2021-08-11 21:26:37 +0200

    Update results_page.dart

--- lib/results_page.dart
+++ lib/results_page.dart
@@ -3,6 +3,11 @@ import 'package:flutter/material.dart';
 class ResultsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('BMI CALCULATOR'),
+      ),
+      body: Text('Hello'),
+    );
   }
 }
